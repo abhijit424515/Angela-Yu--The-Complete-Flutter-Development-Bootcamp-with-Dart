8a677c84a263fbc0c7b7e2423ba81d8964fb0d1a
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2023-03-21 17:19:40 +0530

    added status bar transparency in todoey

--- todoey/lib/main.dart
+++ todoey/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey/providers/task_data.dart';
 import 'package:todoey/screens/tasks_screen.dart';
@@ -12,6 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ));
     return ChangeNotifierProvider(
       create: (context) => TaskData(),
       child: const MaterialApp(
